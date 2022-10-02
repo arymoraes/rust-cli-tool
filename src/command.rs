78456a7cc63d52ab33840d992f28f2e137e982c4
Ary <arylmoraesn@gmail.com>
2022-10-02 14:03:18 -0600

    adds or updates

--- src/command.rs
+++ src/command.rs
@@ -1,3 +1,6 @@
+use std::io::stdin;
+
+use colored::*;
 use rusqlite::{Connection, Result};
 
 #[derive(Debug)]
@@ -8,7 +11,7 @@ pub struct Command {
 }
 
 pub fn add_command(alias: &str, command: &str) -> Result<()> {
-    let conn = Connection::open("./mydb.db3")?;
+    let conn = Connection::open("./data.db3")?;
 
     conn.execute(
         "CREATE TABLE if not exists command (
@@ -19,49 +22,98 @@ pub fn add_command(alias: &str, command: &str) -> Result<()> {
         (),
     )?;
 
-    let new_command = Command {
-        id: 1,
-        alias: alias.to_string(),
-        command: command.to_string(),
-    };
-    conn.execute(
-        "INSERT INTO command (alias, command) VALUES (?1, ?2)",
-        (&new_command.alias, &new_command.command),
-    )?;
+    let command_exists = get_command(alias);
 
-    println!("New command: {:?}", new_command.alias);
+    if command_exists.is_none() {
+        let new_command = Command {
+            id: 1,
+            alias: alias.to_string(),
+            command: command.to_string(),
+        };
 
-    Ok(())
+        conn.execute(
+            "INSERT INTO command (alias, command) VALUES (?1, ?2)",
+            (&new_command.alias, &new_command.command),
+        )?;
+
+        println!(
+            "{} {:?}",
+            "new command added".green(),
+            new_command.alias.cyan()
+        );
+
+        return Ok(());
+    } else {
+        let mut input = String::new();
+
+        println!("{}", "Command already exists. Overwrite? (Y/N)".yellow());
+        stdin()
+            .read_line(&mut input)
+            .ok()
+            .expect("Failed to read line");
+
+        if input.trim() == "Y" {
+            conn.execute(
+                "UPDATE command SET command = ?1 WHERE alias = ?2",
+                (command, alias),
+            )?;
+
+            println!("{} {}", "Command updated:".green(), alias.cyan());
+
+            return Ok(());
+        } else {
+            println!("{}", "Command not updated, exiting program.".red());
+            return Ok(());
+        }
+    }
 }
 
 pub fn run_command(alias: &str) -> Result<()> {
-    let conn = Connection::open("./mydb.db3")?;
+    let command = get_command(alias);
 
-    let query = format!(
-        "SELECT id, alias, command FROM command WHERE alias = '{}'",
-        alias
-    );
-
-    let mut stmt = conn.prepare(&query)?;
-
-    let mut command_iter = stmt.query_map([], |row| {
-        Ok(Command {
-            id: row.get(0)?,
-            alias: row.get(1)?,
-            command: row.get(2)?,
-        })
-    })?;
-
-    // if we have at least one command run it otherwise panic
-    if let Some(command) = command_iter.next() {
-        let command = command.unwrap();
-        println!("Running command: {:?}", command.command);
-        std::process::Command::new("cmd")
-            .args(["/C", &command.command])
-            .output()
-            .unwrap();
-        return Ok(());
+    if command.is_none() {
+        println!(
+            "{} {}, {}",
+            "No command found for alias".red(),
+            alias.yellow(),
+            "You can add one by running add <alias> <command>".red()
+        );
+        return Err(rusqlite::Error::QueryReturnedNoRows);
     }
 
-    panic!("No command found for alias: {}", alias);
+    let command = command.unwrap();
+    println!(
+        "{} {}",
+        "Running command".cyan(),
+        &command.command.bright_green()
+    );
+    std::process::Command::new("cmd")
+        .args(["/C", &command.command])
+        .output()
+        .unwrap();
+    return Ok(());
+}
+
+fn get_command(alias: &str) -> Option<Command> {
+    let conn = Connection::open("./data.db3").unwrap();
+
+    let mut stmt = conn
+        .prepare("SELECT id, alias, command FROM command WHERE alias = ?1")
+        .unwrap();
+
+    let command_iter = stmt
+        .query_map([alias], |row| {
+            Ok(Command {
+                id: row.get(0)?,
+                alias: row.get(1)?,
+                command: row.get(2)?,
+            })
+        })
+        .unwrap();
+
+    for command in command_iter {
+        return Some(command.unwrap());
+    }
+
+    return None;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-// use std::process::Command;
-
+use colored::*;
 use command::run_command;
 
 pub mod command;
@@ -11,18 +10,33 @@ fn main() {
         let alias = std::env::args().nth(2).unwrap();
         // command is everything that comes after the alias:
         let command = std::env::args().skip(3).collect::<Vec<_>>().join(" ");
-        let result = command::add_command(&alias, &command);
-        println!("result: {:?}", result);
+        command::add_command(&alias, &command).unwrap();
         return;
     }
 
     let alias = std::env::args().nth(1);
 
     match alias {
-        Some(_) => {}
-        None => panic!("No alias provided"),
-    }
+        Some(command_alias) => {
+            println!(
+                "{} {}",
+                "Running command for:".cyan(),
+                command_alias.bright_blue()
+            );
+            let result = run_command(&command_alias.as_str());
 
-    let result = run_command(alias.unwrap().as_str());
-    println!("result: {:?}", result);
+            match result {
+                Ok(_) => {
+                    println!("{}", "Command ran successfully".green());
+                }
+                Err(_) => {
+                    println!("{}", "Exited without executing.".red());
+                }
+            }
+        }
+        None => {
+            println!("{}", "No alias provided".red());
+            return;
+        }
+    }
 }
